277d97657ac2472e48f270f7b04ffc581fc59367
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 08:06:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ function wakeDog(dogName, dogBreed) {
   return wakeDog;
 }
 function leashDog(dogName, dogBreed) {
-  console.log("Wake ${dogName} the ${dogBreed}");
+  console.log('Wake ${dogName} the ${dogBreed}');
   return wakeDog;
 }
 function walkToPark(dogName, dogBreed) {
-  console.log("Wake ${dogName} the ${dogBreed}");
+  console.log('Walk ${dogName} the ${dogBreed} to the park');
   return wakeDog;
 }
 function throwFrisbee(dogName, dogBreed) {
-  console.log("Wake ${dogName} the ${dogBreed}");
+  console.log('Throw ${dogName} the ${dogBreed} the frisbee');
   return wakeDog;
 }
 function walkHome(dogName, dogBreed) {
-  console.log("Wake ${dogName} the ${dogBreed}");
+  console.log('Walk ${dogName} the ${dogBreed} home');
   return wakeDog;
 }
-function walkHome(dogName, dogBreed) {
-  console.log("Wake ${dogName} the ${dogBreed}");
+function unleashDog(dogName, dogBreed) {
+  console.log('Unleash ${dogName} the ${dogBreed}');
   return wakeDog;
 }
