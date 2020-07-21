e9914223b67bccc3132d53f0febcd3f6e1202db8
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 08:05:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function wakeDog(dogName, dogBreed) {
   console.log("Wake ${dogName} the ${dogBreed}");
   return wakeDog;
 }
-function wakeDog(dogName, dogBreed) {
+function leashDog(dogName, dogBreed) {
   console.log("Wake ${dogName} the ${dogBreed}");
   return wakeDog;
 }
