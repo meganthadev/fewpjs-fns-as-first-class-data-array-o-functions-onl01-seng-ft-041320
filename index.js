071406e3e3669e5e9ce0ba49cda9acaba32a0c57
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 08:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-const wakeDog = function wakeDog(dogName, dogBreed) {
-  console.log("Wake ${dogName} the ${dogBreed}");
-  return wakeDog;
-}
-function wakeDog(dogName, dogBreed) {
-  console.log("Wake ${dogName} the ${dogBreed}");
-  return wakeDog;
-}
-function leashDog(dogName, dogBreed) {
-  console.log('Wake ${dogName} the ${dogBreed}');
-  return wakeDog;
-}
-function walkToPark(dogName, dogBreed) {
-  console.log('Walk ${dogName} the ${dogBreed} to the park');
-  return wakeDog;
-}
-function throwFrisbee(dogName, dogBreed) {
-  console.log('Throw ${dogName} the ${dogBreed} the frisbee');
-  return wakeDog;
-}
-function walkHome(dogName, dogBreed) {
-  console.log('Walk ${dogName} the ${dogBreed} home');
-  return wakeDog;
-}
-function unleashDog(dogName, dogBreed) {
-  console.log('Unleash ${dogName} the ${dogBreed}');
-  return wakeDog;
- }
-]
-function exerciseDog(dogName, dogBreed) {
-  
-}
+const wakeDog = function(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`
+    }
+
+const leashDog = function(dogName, dogBreed) {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`
+    }
+
+const walkToPark = function(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+    }
+
+const throwFrisbee = function(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
+    }
+
+const walkHome = function(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`
+    }
+
+const unleashDog = function(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`
+    }
