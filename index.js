5b9c558fc3a1d40b5d7f5eab5d65d39b43d61f0a
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 08:04:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ function wakeDog(dogName, dogBreed) {
   console.log("Wake ${dogName} the ${dogBreed}");
   return wakeDog;
 }
+function wakeDog(dogName, dogBreed) {
+  console.log("Wake ${dogName} the ${dogBreed}");
+  return wakeDog;
+}
+function wakeDog(dogName, dogBreed) {
+  console.log("Wake ${dogName} the ${dogBreed}");
+  return wakeDog;
+}
+function wakeDog(dogName, dogBreed) {
+  console.log("Wake ${dogName} the ${dogBreed}");
+  return wakeDog;
+}
+function wakeDog(dogName, dogBreed) {
+  console.log("Wake ${dogName} the ${dogBreed}");
+  return wakeDog;
+}
+function wakeDog(dogName, dogBreed) {
+  console.log("Wake ${dogName} the ${dogBreed}");
+  return wakeDog;
+}
